8f33d1c64353551ff1921b29b12809f23aceb35f
elturanshucai <elturanfcb@gmail.com>
2026-03-11 12:07:55 +0400

    fix

--- src/config/firebase.ts
+++
@@ -1,9 +0,0 @@
-import admin from "firebase-admin";
-import serviceAccount from "../../serviceAccount.json";
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  storageBucket: "portfolioproject-27b50.appspot.com"
-});
-
-export const bucket = admin.storage().bucket();
